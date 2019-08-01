62f4837e9766471641e2ce3d7fc5cc7efd3a0124
Gabriela Velasquez <https://github.com/GabrielaVelasquez06>
2019-08-01 09:32:25 -0600

    ModificoAutenticacion

---
+++ app/src/main/java/com/example/repararapido/LoginActivity.java
@@ -0,0 +1,208 @@
+package com.example.repararapido;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText mEmail,mPassword;
+    TextView notHaveAccountTv,mRecoverPassword;
+    Button mLoginBtn;
+
+    private FirebaseAuth mAuth;
+
+    ProgressDialog pd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Login");
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mEmail = findViewById(R.id.emailEt);
+        mPassword = findViewById(R.id.passwordEt);
+        notHaveAccountTv = findViewById(R.id.nothave_accountTv);
+        mRecoverPassword = findViewById(R.id.recoverPassTv);
+        mLoginBtn = findViewById(R.id.loginbtn);
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = mEmail.getText().toString();
+                String password = mPassword.getText().toString().trim();
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+
+                    mEmail.setError("Invalid Email");
+                    mEmail.setFocusable(true);
+                }
+                else{
+
+                    loginUser(email,password);
+                }
+
+            }
+        });
+
+        notHaveAccountTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
+
+        mRecoverPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRecoverPasswordDialog();
+            }
+        });
+
+        pd = new ProgressDialog(this);
+
+    }
+
+    private void showRecoverPasswordDialog() {
+
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        builder.setTitle("Recover Password");
+
+        LinearLayout linearLayout = new LinearLayout(this);
+
+        final EditText emailEt = new EditText(this);
+        emailEt.setHint("Email");
+        emailEt.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+
+        emailEt.setMinEms(16);
+
+        linearLayout.addView(emailEt);
+        linearLayout.setPadding(10,10,10,10);
+
+        builder.setView(linearLayout);
+
+        builder.setPositiveButton("Recover", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                String email = emailEt.getText().toString().trim();
+                beginRecovery(email);
+
+
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                dialog.dismiss();
+            }
+        });
+
+        builder.create().show();
+
+
+    }
+
+    private void beginRecovery(String email) {
+
+        pd.setMessage("Sending Email...");
+        pd.show();
+
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+
+                pd.dismiss();
+
+                if( task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Email sent", Toast.LENGTH_SHORT).show();
+                }
+                else{
+
+                    Toast.makeText(LoginActivity.this, "Failed....", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                pd.dismiss();
+
+                Toast.makeText(LoginActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loginUser(String email, String password) {
+        pd.setMessage("Logging In...");
+            pd.show();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            pd.dismiss();
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            startActivity(new Intent(LoginActivity.this, ProfileActivity.class));
+                            finish();
+                        } else {
+                            pd.dismiss();
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                Toast.makeText(LoginActivity.this,""+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+}
